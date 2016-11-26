53c24d5d4772a82d192336880a69582226def12e
nathanwallh <nathanwallh@gmail.com>
2016-11-26 13:24:49 +0200

    Add files via upload

--- server.py
+++ server.py
@@ -3,9 +3,26 @@
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
+import re
+from os import mkdir
+from os import path
 
+# Extract users info(name and password) from the file Uinfo.txt.
+# Return them as a list of dicionaries: {username:xxx, password:yyy}.
+def getUsers():
+    ufile = open("Uinfo.txt","r")
+    uinfo = ufile.read()
+    uinfo = re.findall( "AddUser{\n.*?\n}", uinfo, flags=re.DOTALL )
+    uinfo = [ { 'username':x.split("\n")[1], 'password':x.split("\n")[2] } for x in uinfo ]
+    return uinfo
+
+# Add users from Uinfo.txt and create directories for them.
 auth = DummyAuthorizer( )
-auth.add_user( 'nathan', '123', './nathanuser', perm='elradfmw' )
+for x in getUsers():
+    if False == path.exists( x.get('username') ):
+        mkdir( x.get('username') )
+    auth.add_user( x.get('username'), x.get('password'), './'+x.get('username'), perm='elradfmw' )
+
 
 handler = FTPHandler
 handler.authorizer = auth
