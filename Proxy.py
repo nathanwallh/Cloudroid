789338a9313dcd0bcddfc93540c6c59685bb5967
nathanwallh <nathanwallh@gmail.com>
2016-12-17 00:07:18 +0200

    Now proxy is both classy and threaded

--- Proxy.py
+++ Proxy.py
@@ -8,66 +8,71 @@
 # server listens on port 8000.
 # The first server to run should be the FTP server( Server.py )
 # Then, after running the proxy, an FTP client can make a 
-# connection through port 6666 and then things run as usual.
+# connection through port 6000 and then things run as usual.
 
 
+import threading
 import socket
 import FtpNet
 
     
 
+class ProxyThread( threading.Thread ):
+    def __init__( self, client ): 
+        self.client = client
+        self.network = FtpNet.FtpNet('Netinfo.txt')
+        threading.Thread.__init__( self )
+   
+    # Serving the client
+    def run( self ):
+        self.client.send(b'220 FTPnetwork\r\n')
+        while True:
+            # Get input from client and send to network
+            cli_inpt = self.client.recv(256)
+            print( "Client says: ", cli_inpt.decode() ) 
+            self.network.net_send(cli_inpt)
+
+            if not cli_inpt:
+                break
+            cmd = cli_inpt.decode().split()[0]
+            
+            # Get input from network and send to client
+            net_inpt = self.network.net_recv(256)
+             
+            print( "Network says: ", net_inpt.decode() ) 
+            self.client.send( net_inpt )
+
+            if not net_inpt:
+                self.client.close()
+                break
+            
+            code = net_inpt.decode().split()[0]
+            
+            if code == "221":
+                self.client.close()
+                break;
+            # Code 125 
+            elif code == "125":
+                net_inpt = self.network.net_recv( 256 )
+
+
 class ProxyServer:
     def __init__( self, port ):
-        # Listen on port 6000 and wait for clients
+        # Initialize the server socket
         self.sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
         self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind( ('0.0.0.0',port) )
         self.sock.listen( 5 )
 
-    def run( self ):
+    def serve( self ):
         while True:
             # Wait for clients and serve them
             client = self.sock.accept()[0]
-            client.send(b'220 FTPnetwork\r\n')
-            network = FtpNet.FtpNet('Netinfo.txt')
-
-            # Mediate between the client and the network
-            while True:
-                
-                # Get input from client and send to network
-                cli_inpt = client.recv(256)
-                
-                print( "Client says: ", cli_inpt.decode() ) 
-                network.net_send(cli_inpt)
-
-                if not cli_inpt:
-                    break
-                
-                cmd = cli_inpt.decode().split()[0]
-                
-                if cmd == "BYEBYE":
-                    exit()
-                
-                # Get input from network and send to client
-                net_inpt = network.net_recv(256)
-                 
-                print( "Network says: ", net_inpt.decode() ) 
-                client.send( net_inpt )
-
-                if not net_inpt:
-                    client.close()
-                    break
-                
-                code = net_inpt.decode().split()[0]
-                
-                if code == "221":
-                    client.close()
-                    break;
-                # Code 125 
-                elif code == "125":
-                    net_inpt = network.net_recv( 256 )
-
+            serve_thread = ProxyThread( client )
+            serve_thread.daemon = True
+            serve_thread.start()
+            
                     
 if __name__ == '__main__':
     Proxy = ProxyServer(6000)
-    Proxy.run()
+    Proxy.serve()
