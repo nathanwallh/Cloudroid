f888928f57818c2e8fb8596d9790276dc8285f72
nathanwallh <nathanwallh@gmail.com>
2016-12-20 09:41:28 +0200

    this is orens stuff

--- SetupServer.py
+++ SetupServer.py
@@ -1,27 +1,11 @@
-#!/usr/bin/python3
-from sys import argv
-from shutil import rmtree
-from os import path
-import UinfoFunc
-import re
-
-def deldirs( dirlist ):
-    for x in dirlist:
-        if path.exists( x ):
-            rmtree( x )
-
-
-
-introduction = """
 ######################
-SetupServer.py
-Setup the FTP server.
+# SetupServer.py
+# Setup the FTP server.
 ######################
-
+"""
 OPTIONS:
 --------
-
--delall 
+-delall
     Delete all users.
 
 -del <username1> <username2> ...
@@ -30,26 +14,55 @@ OPTIONS:
 -add <username1>:<password1> <username2>:<password2> ...
     Add users.
 """
-if len(argv) == 1:
-    print( introduction )
-    exit()
 
 
+#!/usr/bin/python3
+from shutil import rmtree
+from os import path
+from UinfoFunc import add_new_username, get_all_usernames_from_file, delete_all_users, delete_user
+from argparse import ArgumentParser
 
-if argv[1] == "-delall":
-    uinfo = UinfoFunc.getUsers()
-    deldirs( [ user.get('username') for user in uinfo ] )
-    open("Uinfo.txt", "w").close()
-elif argv[1] == "-del":
-    to_be_deleted = argv[2:]
-    uinfo = UinfoFunc.getUsers()
-    deldirs( [ x for x in argv[2:] ] )
-    new_uinfo = [ user for user in uinfo if user.get('username') not in to_be_deleted ]
-    UinfoFunc.writeUsers( new_uinfo )
-elif argv[1] == "-add":
-    newusers = argv[2:]
-    newusers = [ { 'username':x.split(':',1)[0], 'password':x.split(':',1)[1] } for x in newusers ]
-    UinfoFunc.writeUsers( UinfoFunc.getUsers() + newusers )
-else:
-    print("Error in arguments")
+FILENAME = 'Uinfo.txt'
 
+
+def deldirs(dirlist):
+    for x in dirlist:
+        if path.exists(x):
+            rmtree(x)
+
+
+def argument_parser():
+    parser = ArgumentParser(description='Managing users - Delete and Add User.')
+    parser.add_argument('-delall', type=str, help="Delete all username in file")
+    parser.add_argument('-delete', type=str, help='Deleting the username as enter\r\n'
+                                              '<username1>:<username2>')
+    parser.add_argument('-add', type=str, help='Add the username as enter\r\n'
+                                               '<username1>:<password1> <username2>:<password2>')
+    return parser.parse_args()
+
+
+def main():
+    # print(__doc__)
+    args = argument_parser()
+
+    if args.add is not None:
+        add_new_username(FILENAME, args.add)
+    elif args.delall is not None:
+        delete_all_users(FILENAME)
+    elif args.delete is not None:
+        # Get all the users that need to be deleted
+        delete_users = args.delete
+        # Get all the users that the server
+        all_users = get_all_usernames_from_file(FILENAME)
+
+        delete_users = delete_users.split(":")
+        for user in delete_users:
+            user = user.lower()
+            if user in all_users.keys():
+                all_users.pop(user, None)
+
+        delete_user(all_users, FILENAME)
+
+
+if __name__ == "__main__":
+    main()
